101f342ed3882e704c2d69ec71c32c812fe442b2
A.R. Arun Roshan <arunraksha234@gmail.com>
2023-07-01 13:12:02 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (95.07%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0)
-        {
-            return 0;
+       int f[35];
+        f[0] = 0;
+        f[1] = 1;
+        
+        for(int i=2; i<=n ; i++ ){
+            f[i] = f[i-1] + f[i-2];
         }
-        if(n==1)
-        {
-            return 1;
-        }
-        return fib(n-2)+fib(n-1);
+        return f[n];
     }
 };
