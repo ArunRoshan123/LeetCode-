3e2d46b8d649a75d6656b51476bc0a0b7a01bbe7
A.R. Arun Roshan <arunraksha234@gmail.com>
2023-09-01 19:10:41 +0530

    Time: 4 ms (74.12%), Space: 8.5 MB (16.05%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -8,40 +8,40 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-void reverse(ListNode* &head, ListNode* curr, ListNode* prev)
-{
-    if(curr == NULL)
-    {
-        head = prev;
-        return;
-    }
-    ListNode* forward = curr->next;
-    reverse(head, forward, curr);
-    curr -> next = prev;
+// void reverse(ListNode* &head, ListNode* curr, ListNode* prev)
+// {
+//     if(curr == NULL)
+//     {
+//         head = prev;
+//         return;
+//     }
+//     ListNode* forward = curr->next;
+//     reverse(head, forward, curr);
+//     curr -> next = prev;
     
-}
+// }
+
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* curr = head;
-        ListNode* prev = NULL;
-        reverse(head,curr,prev);
         
-//         if(head == NULL || head->next == NULL)
-//         {
-//             return head;
-//         }
+        if(head == NULL or head->next == NULL)
+        {
+            return head;
+        }
+        ListNode* curr = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
         
-//         ListNode* curr = head;
-//         ListNode* prev = NULL;
-        
-//         while(curr != NULL)
-//         {
-//             ListNode* forward = curr->next;
-//             curr->next = prev;
-//             prev = curr;
-//             curr = forward;
-//         }
-        return head;   
+        return curr;
     }
 };
+
+
+
+
+
+
+
+
+
